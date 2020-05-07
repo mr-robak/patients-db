7c1aff156461975fd9a0dc830a7a0c2dee79107f
Marcin Robak <mr.robak@gmail.com>
2020-05-07 11:53:23 +0200

    patients list fetched

--- src/components/PatientCard.js
+++ src/components/PatientCard.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-export default function patientCard() {
+export default function PatientCard(props) {
+  console.log("props on PatientCard: ", props);
   return (
     <div>
       <h1>Patient Card</h1>
--- src/pages/PatientsDB.js
+++ src/pages/PatientsDB.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
-import patientCard from "../components/patientCard";
+import PatientCard from "../components/PatientCard";
 
 export default function PatientsDB() {
   const initialDoctors = [
@@ -23,23 +23,28 @@ export default function PatientsDB() {
       onDuty: false,
     },
   ];
-  const [doctors, setDoctors] = useState(initialDoctors);
+  const [doctors, setDoctors] = useState([]);
   // const [fetchStatus, setFetchStatus] = useState("");
-  const [selectedDoc, setSelectedDoc] = useState("test");
+  const [selectedDoc, setSelectedDoc] = useState("please select a doctor");
+  const [patients, setPatients] = useState([]);
 
-  const apiUrl =
+  const apiUrlDocs =
     "https://my-json-server.typicode.com/Codaisseur/patient-doctor-data/doctors";
+  const apiUrlPatients =
+    "https://my-json-server.typicode.com/Codaisseur/patient-doctor-data/patients";
 
   useEffect(() => {
     // setFetchStatus("Loading data...");
     const fetchDoctors = async () => {
       //fetch data
-      const fetchedList = await Axios.get(apiUrl);
-      // console.log("fetch list of doctors in DocShedule: ", fetchedList.data);
+      let fetchedList = await Axios.get(apiUrlDocs);
+      // console.log("fetched list of doctors in PatientDB: ", fetchedList.data);
       setDoctors(fetchedList.data);
+      fetchedList = await Axios.get(apiUrlPatients);
+      setPatients(fetchedList.data);
+      console.log("fetched list of patients in PatientDB: ", fetchedList.data);
+
       // setFetchStatus("");
-      // console.log(fetchedList.data[0].doctor);
-      setSelectedDoc(fetchedList.data[0].doctor);
     };
 
     fetchDoctors(); // call fetch()
@@ -61,17 +66,34 @@ export default function PatientsDB() {
       );
     });
   };
-  // console.log(renderOptions());
+
+  const renderPatients = () => {
+    patients.map((patient) => {
+      const { id, firstName, lastName, dateOfBirth } = patient;
+      return (
+        <PatientCard
+          key={id}
+          firstName={firstName}
+          lastName={lastName}
+          dateOfBirth={dateOfBirth}
+        />
+      );
+    });
+  };
+
   return (
     <div>
       <h1>Patients Database</h1>
       <label>
         doctor:
         <select name="doctors" value={selectedDoc} onChange={selectOption}>
+          <option key="-1" value="showing all patients">
+            showing all patients
+          </option>
           {renderOptions()}
         </select>
       </label>
-      <patientCard />
+      {renderPatients()}
     </div>
   );
 }
